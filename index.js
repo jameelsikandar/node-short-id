e1d7b6aa092299f4d77d1cee374a81e90abfc0cb
Jameel Sikandar <jameelsikandarr@gmail.com>
2024-08-20 12:28:25 +0500

    short url

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+const PORT = 8000;
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, ()=>{console.log(`Server started at Port ${PORT}`)});
---
+++ models/url.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+
+const URLSchema = new mongoose.Schema({
+    shortId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    redirectUrl: {
+        type: String,
+        required: true,
+
+    },
+    visitHistory: [{ timestamp: {type: Number}}],
+
+},
+{timestamps: true}
+);
+
+
+const URL = mongoose.model('url', URLSchema);
+
+
+module.exports = URL;
+
